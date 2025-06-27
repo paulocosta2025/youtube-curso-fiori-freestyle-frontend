0948ef8ce57c7d8ac26a610458b6be18fd58b463
paulocosta2025 <paulo.cristiano.costa@gmail.com>
2025-06-27 18:23:14 +0000

    Internacionalizacao (i18n) mensagem por parametro

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,27 +1,25 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/ui/core/mvc/Controller"
 ], 
-    function (Controller,MessageToast){
+    function (Controller){
         "use strict";
 
         return Controller.extend("br.com.petrobras.yspmtestenota.controller.View1", {
             onInit: function() {
+                var oView = this.getView();
+                var oModel = new sap.ui.model.json.JSONModel();
+                oModel.setData({"usuario": {"nome": "Paulo Felipe"}});
+                oView.setModel(oModel);
             },
 
-            onCalcular: function() {
-                var oView = this.getView();
-                var iB1 = parseInt(oView.byId("b1").getValue());
-                var iB2 = parseInt(oView.byId("b2").getValue());
-                var iB3 = parseInt(oView.byId("b3").getValue());
-                var iB4 = parseInt(oView.byId("b4").getValue());
-                var fResultado = 0;
+            onExibirMensagem: function () {
+                var oI18n  = this.getView().getModel("i18n").getResourceBundle();
+                var oModel = this.getView().getModel();
+                var oData  = oModel.getData();
 
-                fResultado = ( iB1 + iB2 + iB3 + iB4 ) / 4;
-
-                oView.byId("resultado").setValue(fResultado);
-
-                MessageToast.show("Resultado = " + fResultado);
+                var sText = oI18n.getText("welcomeMsg",[oData.usuario.nome]);
+                alert(sText);
             }
+
         });
     });
