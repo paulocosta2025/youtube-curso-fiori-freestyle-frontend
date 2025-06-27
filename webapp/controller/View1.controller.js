7a8f008cf70a476f1950a8c53dafcd8da2b7ffdf
paulocosta2025 <paulo.cristiano.costa@gmail.com>
2025-06-27 13:28:13 +0000

    Criação da calculadora de notas

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,14 +1,27 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
-    "use strict";
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], 
+    function (Controller,MessageToast){
+        "use strict";
 
-    return Controller.extend("br.com.petrobras.yspmtestenota.controller.View1", {
-        onInit() {
-        },
+        return Controller.extend("br.com.petrobras.yspmtestenota.controller.View1", {
+            onInit: function() {
+            },
 
-        onPress: function(){    
-            alert("Hello World");
-        }
+            onCalcular: function() {
+                var oView = this.getView();
+                var iB1 = parseInt(oView.byId("b1").getValue());
+                var iB2 = parseInt(oView.byId("b2").getValue());
+                var iB3 = parseInt(oView.byId("b3").getValue());
+                var iB4 = parseInt(oView.byId("b4").getValue());
+                var fResultado = 0;
+
+                fResultado = ( iB1 + iB2 + iB3 + iB4 ) / 4;
+
+                oView.byId("resultado").setValue(fResultado);
+
+                MessageToast.show("Resultado = " + fResultado);
+            }
+        });
     });
-});
